ec1c2bc39192f09e896714cd67e94bce8cb8b1e2
Khashayar Hajnabi <Khashayar.hajnabi@gmail.com>
2022-06-04 11:59:24 +0430

    Created the initial files.

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const port = 3000;
+const path = require("path")
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+
+app.listen(port, () => {
+    console.log(`The server is running succesfully on the http://localhost:${port}. `);
+});
+
+
+// serving the static files
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// serving the / route
+app.get("/", (req, res) => {
+    res.sendFile("./public/index.html", { root: __dirname });
+});
+
+
+
+
+
+// serving not found urls
+app.use((req, res) => {
+    res.send("Page is not found!")
+});
+  
